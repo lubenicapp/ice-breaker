10a2f989e3528eec95407da8d14e641e2261f5bb
vo <v.o@tuta.com>
2024-09-24 21:16:04 +0200

    refacto view

--- network/views.py
+++ network/views.py
@@ -18,31 +18,57 @@ class NetworkViewSet(CreateModelMixin, GenericViewSet):
 
 @api_view(['GET', 'POST'])
 def person_view(request):
-    slug = request.headers.get('Network-Identifier', None)
-    network = Network.objects.filter(slug=slug).first()
-    if not slug or not network:
+    # Get network authorization
+    network = get_network_from_headers(request)
+    if not network:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
     if request.method == 'GET':
-        persons = Person.objects.filter(networks=network)
-        serializer = PersonRetrieveListSerializer(persons, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return handle_get_request(network)
 
     if request.method == 'POST':
-        linkedin_identifier = request.data.get('linkedin_identifier', None)
-        if not linkedin_identifier:
-            return Response({'error': 'linkedin_identifier is required'}, status=status.HTTP_400_BAD_REQUEST)
+        return handle_post_request(request, network)
 
-        # keeps "joe" from https://www.linkedin.com/in/joe?tracker_id=1555
-        identifier_from_url = linkedin_identifier.strip('/').split('/')[-1].split('?')[0]
 
+def get_network_from_headers(request):
+    slug = request.headers.get('Network-Identifier', None)
+    if slug:
+        return Network.objects.filter(slug=slug).first()
+    return None
+
+
+def handle_get_request(network):
+    persons = Person.objects.filter(networks=network)
+    serializer = PersonRetrieveListSerializer(persons, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def handle_post_request(request, network):
+    linkedin_identifier = request.data.get('linkedin_identifier', None)
+    if not linkedin_identifier:
+        return Response({'error': 'linkedin_identifier is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    identifier_from_url = extract_identifier_from_url(linkedin_identifier)
+
+    person = get_person_or_ingest(identifier_from_url)
+    if not person:
+        return Response({'error': 'Unable to create person'}, status=status.HTTP_400_BAD_REQUEST)
+
+    person.networks.add(network)
+    person.save()
+
+    serializer = PersonRetrieveListSerializer(person)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def extract_identifier_from_url(linkedin_identifier):
+    return linkedin_identifier.strip('/').split('/')[-1].split('?')[0]
+
+
+def get_person_or_ingest(identifier_from_url):
+    person = Person.objects.filter(linkedin_identifier=identifier_from_url).first()
+    if not person:
+        profile_data = LinkedinScraper.get_profile(f'https://www.linkedin.com/in/{identifier_from_url}')
+        ProfileParser.ingest_profile_data(profile_data)
         person = Person.objects.filter(linkedin_identifier=identifier_from_url).first()
-        if not person:
-            ProfileParser.ingest_profile_data(
-                LinkedinScraper.get_profile(f'https://www.linkedin.com/in/{identifier_from_url}')
-            )
-            person = Person.objects.filter(linkedin_identifier=identifier_from_url).first()
-        person.networks.add(network)
-        person.save()
-        serializer = PersonRetrieveListSerializer(person)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    return person
