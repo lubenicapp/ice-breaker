ea5cdc3e8907f5805ff351a4428516b413c963ac
vo <v.o@tuta.com>
2024-09-27 04:13:47 +0200

    cors localhost

--- app/settings.py
+++ app/settings.py
@@ -34,7 +34,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    'https://gregarious-ganache-18e07f.netlify.app'
+    'https://gregarious-ganache-18e07f.netlify.app',
+    'http://localhost:5173',
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
